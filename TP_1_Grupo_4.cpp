4d159167290e5eb7a8f7eb1f67503cd6528d49a6
Julian Pampín <jpampin@frba.utn.edu.ar>
2025-09-25 21:00:02 -0300

    Update TP_1_Grupo_4.cpp

--- TP_1_Grupo_4.cpp
+++ TP_1_Grupo_4.cpp
@@ -1,7 +1,6 @@
-#include iostream 
-#include fstream
+#include <iostream>
+#include <fstream>
 #include "rwstring.hpp"
-#include "listas.hpp"
 using namespace std;
 
 const int dimprod = 10;
@@ -12,6 +11,9 @@ const int dimclie = 15;
 const int dimtipoP = 3; 
 const int dimCost= 4;
 const int dimpresup= 4;
+const int dimtp=3;
+
+const string FIN= "EOF";
 
 struct Product {
     string sku;
@@ -25,14 +27,14 @@ struct Repa {
     float Cost = 0;
     float presup = 0;};
 
-ifstream& operator>>(ifstream &fs, Product &p) {
+fstream& operator>>(fstream &fs, Product &p) {
     p.sku  = readstring(fs, dimsku);
     p.desc = readstring(fs, dimdesc);
     fs.read(reinterpret_cast<char *>(&p.costoF), sizeof(dimcostF));
     return fs;
 }
 
-ifstream& operator>>(ifstream &fs, Repa &r) {
+fstream& operator>>(fstream &fs, Repa &r) {
     r.cliente = readstring(fs, dimclie);
     fs.read(reinterpret_cast<char *>(&r.tipoP), sizeof(dimtipoP));
     r.sku = readstring(fs, dimsku);
@@ -41,28 +43,112 @@ ifstream& operator>>(ifstream &fs, Repa &r) {
     return fs;
 }
 
+bool criterio(const Repa &a, const Repa &b) {
+    if (a.cliente != b.cliente) {
+        return a.cliente < b.cliente; // ordena por cliente
+    }
+    if (a.tipoP != b.tipoP) {
+        return a.tipoP < b.tipoP;   //  mismo cliente, por tipo
+    }
+    return a.sku < b.sku;  //  mismo cliente y tipo, por sku
+}
+
+template <typename T>
+void ordenar(T rep[], int dimrep, bool (*criterio)(const T&, const T&)) {
+    bool desordenado = true;
+    for (int i = 1; i < dimprod && desordenado; i++) {
+        desordenado = false;
+        for (int j = 0; j < dimprod - i; j++) {
+            if (criterio(rep[j + 1], rep[j])) {
+                T aux = rep[j];
+                rep[j] = rep[j + 1];
+                rep[j + 1] = aux;
+                desordenado = true;}
+        } } }
+
+
+int buscar_linea_ord(string clave, Repa rep[], int dimrep){
+    int i;
+    for (i=0; i<dimrep && rep[i].cliente != clave; i++);
+    return(i<dimrep && rep[i].cliente == clave)?i:-1;
+}
+
+int buscar_linea_ord2(string aux, Product prod []){
+    int i;
+    for (i=0; i<dimprod && prod[i].sku != aux; i++);
+    return(i<dimprod && prod[i].sku == aux)?i:-1;
+}
+
+ostream& operator <<(ostream& os, const Repa& a) {
+    string tp [dimtp] {"Electronico", "Mecanico", "Mecatronico"};
+    os << "Cliente: "<<a.cliente<<", Tipo de product:"<< tp[a.tipoP]
+       <<", SKU: "<< a.sku<<", costo:"<<a.Cost<<", Presupuesto:"<<a.presup;
+    return os;
+}
+ostream& operator <<(ostream& os, const Product& a) {
+    os <<"Descripción:"<< a.desc
+       <<", Costo Final: "<< a.costoF;
+    return os;
+}
+
+void Cliente (Repa rep[], int dimrep, Product prod []){ 
+    string clave;
+    cout << "Ingresar el nombre del Cliente: ";
+    cin >> clave;
+    if(clave == FIN)
+        return;
+
+    int iclien = buscar_linea_ord (clave, rep, dimrep);
+    if (iclien==-1){
+        cout<<"Cliente no encontrado"<<endl;
+        return;
+    } //ejercicio 5
+    
+    float CostoFijo =0, CostoDirecto=0, Presupuesto=0; 
+    while(rep[iclien].cliente==clave){ 
+        //Ejercicio 3
+        cout<<rep[iclien];
+        string aux= rep[iclien].sku;
+        int isku= buscar_linea_ord2(aux, prod);
+        cout<<prod[isku];
+
+        //Ejercicio 4
+        CostoFijo+= prod[isku].costoF;
+        CostoDirecto+= rep[iclien].Cost;
+        Presupuesto+= rep[iclien].presup;
+
+        iclien++;
+    }
+    cout<<"La ganancia total de una reparación es de "<<CostoFijo + CostoDirecto - Presupuesto <<"$"<<endl;
+     return Cliente (rep,dimrep,prod);}
+
+
 int main(){
- int dim rep=0;
+ int dimrep=0;
  Product prod [dimprod];
- Repa rep [dim];
+ Repa rep [dimrep];
+
 
  //Ejercicio 2
- ifstream archiprod; 
+ fstream archiprod; 
  archiprod.open("productos.bin", ios::binary);   
-     for (int i = 0; i < 10; i++) {
+     for (int i = 0; i < dimprod; i++) {
         archiprod >> prod [i];         // usa el operador >> que creaste
     }
  archiprod.close();
 
- ifstream archirep; 
+ fstream archirep; 
  archirep.open("reparaciones.bin", ios::binary);
-
-    if(archirep>>) {
-        dim++;
-    }
-
-
-
+    Repa aux; 
+    do {
+    archirep>>rep[dimrep];
+    dimrep++;
+    } while (archirep>>aux);
  archirep.close();
 
+ ordenar(rep, dimrep,*criterio);
+ 
+ Cliente (rep, dimrep, prod);
+
+ return 0;
 }
